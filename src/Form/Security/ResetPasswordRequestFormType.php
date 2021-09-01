951c328dd5c629b2118af92349417981e3d69b5a
Arek Wójcik <a.wojcik@arris.pl>
2021-09-01 20:30:46 +0200

    Reset email label

--- src/Form/Security/ResetPasswordRequestFormType.php
+++ src/Form/Security/ResetPasswordRequestFormType.php
@@ -9,7 +9,6 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Email;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ResetPasswordRequestFormType extends AbstractType
 {
@@ -20,6 +19,10 @@ class ResetPasswordRequestFormType extends AbstractType
             ->add('email', EmailType::class, [
                 'constraints' => [
                     new Email(),
+                ],
+                'attr' => [
+                    'placeholder' => 'user.email',
+                    'autofocus' => true,
                 ],
             ]);
     }
